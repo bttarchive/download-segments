a452724d3d7b24e9864bcb38d5e86de125e53383
Matze <47565631+bttarchive@users.noreply.github.com>
2023-04-12 14:23:23 +0200

    Add files via upload

---
+++ dl.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+from yt_dlp import YoutubeDL
+import subprocess
+import sys
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "yt_dlp"])
+
+with open('dl.txt', "r") as f:
+    lines = [line.rstrip().split('\t') for line in f]
+
+for i in range(0, len(lines)):
+    if len(lines[i]) == 1:
+        lines[i].append('Clip #' + str(i + 1))
+
+with open('failed.txt', 'w') as g:
+    for i in range(0, len(lines)):
+        try:
+            print('Trying to download video', i + 1, 'out of', len(lines))
+            filename = lines[i][1]
+            ydl_opts = {
+                'format': '(bv*[fps>30][height>720]/bv*[fps>30][height<=720]/bv*[height>720]/bv*/best)[ext=mp4] / ' +
+                          'bv*[fps>30][height>720]/bv*[fps>30][height<=720]/bv*[height>720]/bv*/best',
+                'outtmpl': filename + '.%(ext)s',
+            }
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download([lines[i][0]])
+        except:
+            print('Video', i + 1, 'failed to download')
+            g.write(str(i + 1))
+            g.write(' - ')
+            g.write(str(lines[i][0]))
+            g.write(' - ')
+            g.write(str(lines[i][1]))
+            g.write('\n')
