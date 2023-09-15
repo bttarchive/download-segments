5d5bdcb1c94b2ecb2a4ad99c26cc0683dbe9c1ac
Matze <47565631+bttarchive@users.noreply.github.com>
2023-09-15 17:20:53 +0200

    Update dl.py

--- dl.py
+++ dl.py
@@ -5,27 +5,23 @@ import sys
 with open('dl.txt', 'r', encoding="utf8") as e:
     offset = 0
     pp = 0
+    fps = 0
     check = [line.rstrip().split('\t') for line in e]
-    if check[len(check) - 1] == ['UP']:
+    last_row = str(check[len(check) - 1])
+    if 'OPTIONS' in last_row:
         offset = 1
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-U", "setuptools", "wheel"])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--force-reinstall",
-                               "https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz"])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "ffmpeg-python"])
-    elif check[len(check) - 1] == ['CV']:
-        offset = 1
-        pp = 1
-    elif check[len(check) - 1] == ['UP+CV']:
-        offset = 1
-        pp = 1
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-U", "setuptools", "wheel"])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--force-reinstall",
-                               "https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz"])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "ffmpeg-python"])
+        if 'CV' in last_row:
+            pp = 1
+        if 'UP' in last_row:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "-U", "setuptools", "wheel"])
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "--force-reinstall",
+                                   "https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz"])
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "ffmpeg-python"])
+        if 'FPS' in last_row:
+            fps = 1
     else:
-        print("Not updating/converting")
+        print("No additional options given")
 
 from yt_dlp import YoutubeDL
 from yt_dlp.utils import download_range_func
@@ -63,25 +59,53 @@ def download_segments():
                         start_second = max(int(timestamp) - 5, 0)
                         end_second = int(timestamp) + int(lines[i][2]) + 5
                         if pp == 1:
-                            ydl_opts = {
-                                'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
-                                          'height>720]+ba/bv*+ba/best',
-                                'outtmpl': filename + '.%(ext)s',
-                                'downloader': 'ffmpeg',
-                                'download_ranges': download_range_func(None, [(start_second, end_second)]),
-                                'postprocessors': [{
-                                    'key': 'FFmpegVideoConvertor',
-                                    'preferedformat': 'mp4'
-                                }],
-                            }
+                            if fps == 1:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'download_ranges': download_range_func(None, [(start_second, end_second)]),
+                                    'downloader_args': {'ffmpeg': ['-filter:v', 'fps=60', '-vcodec', 'h264']},
+                                    'postprocessors': [{
+                                        'key': 'FFmpegVideoConvertor',
+                                        'preferedformat': 'mp4'
+                                    }],
+                                }
+                            else:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'download_ranges': download_range_func(None, [(start_second, end_second)]),
+                                    'postprocessors': [{
+                                        'key': 'FFmpegVideoConvertor',
+                                        'preferedformat': 'mp4'
+                                    }],
+                                }
                         else:
-                            ydl_opts = {
-                                'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
-                                          'height>720]+ba/bv*+ba/best',
-                                'outtmpl': filename + '.%(ext)s',
-                                'downloader': 'ffmpeg',
-                                'download_ranges': download_range_func(None, [(start_second, end_second)]),
-                            }
+                            if fps == 1:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'download_ranges': download_range_func(None, [(start_second, end_second)]),
+                                    'downloader_args': {'ffmpeg': ['-filter:v', 'fps=60', '-vcodec', 'h264']},
+                                }
+                            else:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'download_ranges': download_range_func(None, [(start_second, end_second)]),
+                                }
                         with YoutubeDL(ydl_opts) as ydl:
                             ydl.download([lines[i][0]])
 
@@ -93,34 +117,112 @@ def download_segments():
                         # Twitch VODs suck, so I hope this is enough offset to make it work
                         end_ts = int(ts_hour)*3600 + int(ts_minute)*60 + int(ts_second) + int(lines[i][2]) + 60
                         if pp == 1:
-                            ydl_opts = {
-                                'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
-                                          'height>720]+ba/bv*+ba/best',
-                                'outtmpl': filename + '.%(ext)s',
-                                'downloader': 'ffmpeg',
-                                'download_ranges': download_range_func([], [(start_ts, end_ts)]),
-                                'postprocessors': [{
-                                    'key': 'FFmpegVideoConvertor',
-                                    'preferedformat': 'mp4'
-                                }],
-                            }
+                            if fps == 1:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'download_ranges': download_range_func([], [(start_ts, end_ts)]),
+                                    'downloader_args': {'ffmpeg': ['-filter:v', 'fps=60', '-vcodec', 'h264']},
+                                    'postprocessors': [{
+                                        'key': 'FFmpegVideoConvertor',
+                                        'preferedformat': 'mp4'
+                                    }],
+                                }
+                            else:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'download_ranges': download_range_func([], [(start_ts, end_ts)]),
+                                    'postprocessors': [{
+                                        'key': 'FFmpegVideoConvertor',
+                                        'preferedformat': 'mp4'
+                                    }],
+                                }
                         else:
-                            ydl_opts = {
-                                'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
-                                          'height>720]+ba/bv*+ba/best',
-                                'outtmpl': filename + '.%(ext)s',
-                                'downloader': 'ffmpeg',
-                                'download_ranges': download_range_func([], [(start_ts, end_ts)]),
-                            }
+                            if fps == 1:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'download_ranges': download_range_func([], [(start_ts, end_ts)]),
+                                    'downloader_args': {'ffmpeg': ['-filter:v', 'fps=60', '-vcodec', 'h264']},
+                                }
+                            else:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'download_ranges': download_range_func([], [(start_ts, end_ts)]),
+                                }
                         with YoutubeDL(ydl_opts) as ydl:
                             ydl.download([lines[i][0]])
 
                     else:
                         if pp == 1:
+                            if fps == 1:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'downloader_args': {'ffmpeg': ['-filter:v', 'fps=60', '-vcodec', 'h264']},
+                                    'postprocessors': [{
+                                        'key': 'FFmpegVideoConvertor',
+                                        'preferedformat': 'mp4'
+                                    }],
+                                }
+                            else:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'postprocessors': [{
+                                        'key': 'FFmpegVideoConvertor',
+                                        'preferedformat': 'mp4'
+                                    }],
+                                }
+                        else:
+                            if fps == 1:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                    'downloader': 'ffmpeg',
+                                    'downloader_args': {'ffmpeg': ['-filter:v', 'fps=60', '-vcodec', 'h264']},
+                                }
+                            else:
+                                ydl_opts = {
+                                    'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                              'height>720]+ba/bv*+ba/best',
+                                    'format_sort': ['proto'],
+                                    'outtmpl': filename + '.%(ext)s',
+                                }
+                        with YoutubeDL(ydl_opts) as ydl:
+                            ydl.download([lines[i][0]])
+
+                except:
+                    if pp == 1:
+                        if fps == 1:
                             ydl_opts = {
                                 'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
                                           'height>720]+ba/bv*+ba/best',
+                                'format_sort': ['proto'],
                                 'outtmpl': filename + '.%(ext)s',
+                                'downloader': 'ffmpeg',
+                                'downloader_args': {'ffmpeg': ['-filter:v', 'fps=60', '-vcodec', 'h264']},
                                 'postprocessors': [{
                                     'key': 'FFmpegVideoConvertor',
                                     'preferedformat': 'mp4'
@@ -130,28 +232,30 @@ def download_segments():
                             ydl_opts = {
                                 'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
                                           'height>720]+ba/bv*+ba/best',
+                                'format_sort': ['proto'],
+                                'outtmpl': filename + '.%(ext)s',
+                                'postprocessors': [{
+                                    'key': 'FFmpegVideoConvertor',
+                                    'preferedformat': 'mp4'
+                                }],
+                            }
+                    else:
+                        if fps == 1:
+                            ydl_opts = {
+                                'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                          'height>720]+ba/bv*+ba/best',
+                                'format_sort': ['proto'],
+                                'outtmpl': filename + '.%(ext)s',
+                                'downloader': 'ffmpeg',
+                                'downloader_args': {'ffmpeg': ['-filter:v', 'fps=60', '-vcodec', 'h264']},
+                            }
+                        else:
+                            ydl_opts = {
+                                'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
+                                          'height>720]+ba/bv*+ba/best',
+                                'format_sort': ['proto'],
                                 'outtmpl': filename + '.%(ext)s',
                             }
-                        with YoutubeDL(ydl_opts) as ydl:
-                            ydl.download([lines[i][0]])
-
-                except:
-                    if pp == 1:
-                        ydl_opts = {
-                            'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
-                                      'height>720]+ba/bv*+ba/best',
-                            'outtmpl': filename + '.%(ext)s',
-                            'postprocessors': [{
-                                'key': 'FFmpegVideoConvertor',
-                                'preferedformat': 'mp4'
-                            }],
-                        }
-                    else:
-                        ydl_opts = {
-                            'format': 'bv*[fps>30][height>720]+ba/bv*[fps>30][height<=720]+ba/bv*['
-                                      'height>720]+ba/bv*+ba/best',
-                            'outtmpl': filename + '.%(ext)s',
-                        }
                     with YoutubeDL(ydl_opts) as ydl:
                         ydl.download([lines[i][0]])
 
